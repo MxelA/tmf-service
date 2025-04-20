838c75b9d9f010d98ca01318fe09bb923898d3a3
Aleksandar Milic <milicalex@gmail.com>
2025-04-20 20:30:47 +0200

    .

---
+++ internal/app/app.go
@@ -0,0 +1,58 @@
+package app
+
+import (
+	//"context"
+	"fmt"
+	"github.com/MxelA/tmf-service/internal/core"
+	//"os"
+	//"os/signal"
+	//"syscall"
+)
+
+type App interface {
+	Serve()
+}
+
+// app contain all the core dependency and should
+// be implement at application startup.
+type app struct {
+	Addr string
+
+	DB *core.DatabaseNeo4j
+	//API    *core.API
+	Logger *core.Logger
+	//PubSub *core.PubSub
+}
+
+func (app *app) Serve() {
+	//api := app.API.GetCore()
+	logger := app.Logger.GetCore()
+	//pubSub := app.PubSub.GetCore()
+
+	logger.Info("App running", "address", app.Addr)
+	//go func() { _ = api.Listen(app.Addr) }()
+
+	//var sig os.Signal
+	//c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
+	//signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
+	//sig = <-c                                       // This blocks the main thread until an interrupt is received
+	//
+	//logger.Info("Signal received", "signal", sig.String())
+	//logger.Info("Shutting down app, waiting background process to finish")
+	//defer logger.Info("App shutdown")
+	//
+	//_ = api.ShutdownWithContext(context.Background())
+	//_ = pubSub.Close()
+}
+
+// // New would implement the App interface by
+// // initialize the app's core dependencies and packages.
+func New(host, port string) App {
+	var app = new(app)
+
+	app.Addr = fmt.Sprintf("%s:%s", host, port)
+	app.initCore()
+	app.initPackages()
+
+	return app
+}
---
+++ internal/app/init.go
@@ -0,0 +1,37 @@
+package app
+
+import "github.com/MxelA/tmf-service/internal/core"
+
+// initCore initialize core packages of application
+// such as router, logger, database, etc.
+func (app *app) initCore() {
+	logger := core.NewLogger()
+	app.Logger = logger
+
+	// db := core.NewDatabase(logger)
+	// app.DB = db
+
+	//api := core.NewApi(logger)
+	//middleware.InitAPIMiddleware(api, slogfiber.New(logger.GetCore()))
+	//app.API = api
+
+	//pubSub := core.NewPubSub(logger)
+	//middleware.InitPubSubMiddleware(pubSub)
+	//app.PubSub = pubSub
+
+	defer logger.GetCore().Info("Initialize dependencies done!")
+}
+
+// initPackages initialize all the packages inside the pkg directory.
+// This function act as single source where all
+// packages should be initialized.
+func (app *app) initPackages() {
+	var (
+		//db     = app.DB
+		//api    = app.API
+		//pubSub = app.PubSub
+		logger = app.Logger
+	)
+
+	defer logger.GetCore().Info("Initialize packages done!")
+}
