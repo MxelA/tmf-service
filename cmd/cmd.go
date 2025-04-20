30c9d96323716c4dcd37ae884d2a33677e8d56e7
Aleksandar Milic <milicalex@gmail.com>
2025-04-20 20:58:57 +0200

    .

---
+++ cmd/cmd.go
@@ -0,0 +1,23 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var (
+	name string
+
+	host string
+	port string
+
+	ext string
+)
+
+var rootCmd = &cobra.Command{Use: "app"}
+
+func init() {
+	rootCmd.AddCommand(apiCmd)
+
+	apiCmd.Flags().StringVar(&port, "port", "8080", "Port to run the API server")
+	apiCmd.Flags().StringVar(&host, "host", "0.0.0.0", "Host to run the API server")
+
+	rootCmd.Execute()
+}
