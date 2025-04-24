ff817a536e3e2df0c9d6c1219e8489e782dd31e2
Aleksandar <milicalex@gmail.com>
2025-04-24 14:52:54 +0200

    remove src folder

--- src/oapi_codegen/tmf638v5/gen.go
+++
@@ -1,5 +0,0 @@
-package tmf638v5
-
-//go:generate rm -rf server
-//go:generate mkdir -p server
-//go:generate oapi-codegen -generate types,server -package myapi TMF638-Service_Inventory_Management-v5.0.0.oas.yaml > server/server.gen.go
