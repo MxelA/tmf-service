5cf0f2e7914d4b689f336bcec61d33a794f0cb64
Aleksandar Milic <milicalex@gmail.com>
2025-08-12 19:58:59 +0200

    .

--- internal/pkg/tmf-service-order/swagger/tmf641v4_2/gen.go
+++ internal/pkg/tmf-service-order/swagger/tmf641v4_2/gen.go
@@ -1,4 +1,4 @@
-package tmf638v4_2
+package tmf641v4_2
 
 //go:generate mkdir -p server
 //go:generate swagger generate server --quiet --target server --name tmf-service-order-v4_2 --spec TMF641-ServiceOrdering-v4.2.0.json --template-dir=./templates --exclude-main
